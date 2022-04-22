d192e7746fae9537f26828b78bf448e6699098fa
royne <royne.chaves@gmail.com>
2022-04-21 23:30:04 -0500

    add index escorts

--- app/serializers/escort_serializer.rb
+++ app/serializers/escort_serializer.rb
@@ -1,4 +1,14 @@
 class EscortSerializer < ActiveModel::Serializer
-  attributes :id, :username, :first_name, :last_name, :city, :description, :price, :stars, :sex, :age, :phone
+  include Rails.application.routes.url_helpers
+
+  attributes :id, :username, :first_name, :last_name, :city, :description, :price, :stars, :sex, :age, :phone, :avatar, :photos
   has_one :user
+
+  def avatar
+     object.avatar.attached? ? url_for(object.avatar) : ""
+  end
+
+  def photos
+    object.photos.attached? ? object.photos.map { |x| url_for(x) } : [] 
+  end
 end
