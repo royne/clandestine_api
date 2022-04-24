95ca688755d23f9919561f2b4ca91a4f84b9e413
royne <royne.chaves@gmail.com>
2022-04-24 18:14:37 -0500

    fix deploy staging 2.0

--- app/serializers/escort_serializer.rb
+++ app/serializers/escort_serializer.rb
@@ -13,6 +13,6 @@ class EscortSerializer < ActiveModel::Serializer
   end
 
    def transform_image(image, dimension)
-    rails_representation_url(image.variant(resize_to_limit: [dimension, nil]), only_path: false)
+    url_for(image.variant(resize_to_limit: [dimension, nil]))
    end
 end
