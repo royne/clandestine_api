670dd9764f289d0dd9fcfd264d8ea776428dc7da
royne <royne.chaves@gmail.com>
2022-05-07 17:38:50 -0500

    fix search

--- app/controllers/api/v1/search_controller.rb
+++ app/controllers/api/v1/search_controller.rb
@@ -4,8 +4,8 @@ module Api
 
       def index
         escorts = params[:category_ids].present? ? Escort.joins(:categories).where("categories.id = ?", params[:category_ids]) : Escort.all
-        escorts = a.joins(:activities).where("activities.id = ?", params[:activity_ids]) if params[:activity_ids].present? 
-        escorts = a.joins(:locations).where("locations.id = ?", params[:location_ids]) if params[:location_ids].present? 
+        escorts = escorts.joins(:activities).where("activities.id = ?", params[:activity_ids]) if params[:activity_ids].present? 
+        escorts = escorts.joins(:locations).where("locations.id = ?", params[:location_ids]) if params[:location_ids].present? 
         render json: escorts
         
       end
